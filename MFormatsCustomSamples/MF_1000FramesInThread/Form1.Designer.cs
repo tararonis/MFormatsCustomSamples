5188881b08f1b8bbe620c21b457c6038dfb26089
tararonis <tararonis@gmail.com>
2019-06-24 12:11:16 +0200

    finish sample 1000frames

--- MFormatsCustomSamples/MF_1000FramesInThread/Form1.Designer.cs
+++ MFormatsCustomSamples/MF_1000FramesInThread/Form1.Designer.cs
@@ -28,13 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.panelPrev = new System.Windows.Forms.Panel();
+            this.frameN = new System.Windows.Forms.Label();
+            this.openFile_btn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(77, 342);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(340, 23);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.Start_btn_Click);
+            // 
+            // panelPrev
+            // 
+            this.panelPrev.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelPrev.Location = new System.Drawing.Point(77, 22);
+            this.panelPrev.Name = "panelPrev";
+            this.panelPrev.Size = new System.Drawing.Size(340, 294);
+            this.panelPrev.TabIndex = 1;
+            // 
+            // frameN
+            // 
+            this.frameN.AutoSize = true;
+            this.frameN.Location = new System.Drawing.Point(434, 117);
+            this.frameN.Name = "frameN";
+            this.frameN.Size = new System.Drawing.Size(31, 13);
+            this.frameN.TabIndex = 2;
+            this.frameN.Text = "0000";
+            // 
+            // openFile_btn
+            // 
+            this.openFile_btn.Location = new System.Drawing.Point(77, 372);
+            this.openFile_btn.Name = "openFile_btn";
+            this.openFile_btn.Size = new System.Drawing.Size(340, 23);
+            this.openFile_btn.TabIndex = 3;
+            this.openFile_btn.Text = "OpenFile";
+            this.openFile_btn.UseVisualStyleBackColor = true;
+            this.openFile_btn.Click += new System.EventHandler(this.OpenFile_btn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(556, 399);
+            this.Controls.Add(this.openFile_btn);
+            this.Controls.Add(this.frameN);
+            this.Controls.Add(this.panelPrev);
+            this.Controls.Add(this.start_btn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Panel panelPrev;
+        private System.Windows.Forms.Label frameN;
+        private System.Windows.Forms.Button openFile_btn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- MFormatsCustomSamples/MF_1000FramesInThread/Form1.cs
+++ MFormatsCustomSamples/MF_1000FramesInThread/Form1.cs
@@ -4,17 +4,120 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MFORMATSLib;
 namespace MF_1000FramesInThread
 {
     public partial class Form1 : Form
     {
+        MFPreviewClass m_objPreview;
+        MFReaderClass m_objReader;
+        MFWriterClass m_objWriter;
+
+        string openPath = @"M:\TEST_VIDEOS\Moneyball.2011.720p.BluRay.x264-HiDt.mkv";
+        string savePath = @"M:\TempVideo\1000frames.mp4";
+
+        bool start;
+
+        public delegate void InvokeDelegate();
+
+        private CancellationTokenSource cancelSource;
+        private Thread m_threadWorker;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_objPreview = new MFPreviewClass();
+            m_objReader = new MFReaderClass();
+            m_objWriter = new MFWriterClass();
+
+            m_objPreview.PreviewWindowSet("", panelPrev.Handle.ToInt32());
+            m_objPreview.PreviewEnable("", 1, 1);
+
+            cancelSource = new CancellationTokenSource();
+            m_threadWorker = new Thread(() => thread_DoWork(cancelSource.Token));
+            m_threadWorker.Name = "thread_DoWork";
+            m_threadWorker.Start();
+        }
+
+        DateTime endTime;
+        int count = 0;
+        private void thread_DoWork(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                if (start)
+                {
+                    count++;
+                    if (count >= 1000 && count <= 2000)
+                    {
+                        MFFrame frame;
+                        m_objReader.SourceFrameGet(-1, out frame, "");                       
+                        m_objPreview.ReceiverFramePut(frame,0, "");
+                        m_objWriter.ReceiverFramePut(frame, -1, "");
+
+                        Marshal.ReleaseComObject(frame);
+                        
+                    }
+                    if (count > 2000)
+                    {
+                        endTime = DateTime.Now;
+                        m_objWriter.WriterClose(0);
+                        m_objReader.ReaderClose();                       
+                       
+                        BeginInvoke(new InvokeDelegate(UpdateButton));
+                        start = false;
+                    }
+                    BeginInvoke(new InvokeDelegate(UpdateLabel));
+                }
+            }
+            }
+
+        private void UpdateLabel()
+        {
+            frameN.Text = count.ToString();
+        }
+
+        private void UpdateButton()
+        {
+            int dif = (endTime.Millisecond + endTime.Second * 1000 + endTime.Minute * 60 * 1000) - (startTime.Millisecond + startTime.Second * 1000 + startTime.Minute * 60 * 1000);
+            start_btn.Text = dif.ToString() + " ms";
+        }
+
+        DateTime startTime;
+        private void Start_btn_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK && saveFileDialog1.FileName != string.Empty)
+            {
+                savePath = saveFileDialog1.FileName;
+                count = 0;
+                startTime = DateTime.Now;
+                m_objReader.PropsSet("experimental.mfcodecs", "true");
+                m_objReader.PropsSet("experimental.out_video_packets", "true");
+                m_objReader.PropsSet("external_process", "false");
+                m_objReader.ReaderOpen(openPath, "");
+
+                m_objWriter.PropsSet("external_process", "false");
+                m_objWriter.WriterSet(savePath, 0, "format='mp4' video::codec='packets' audio::codec='audio_packets'"); // 
+
+                start = true;
+            }
+        }
+
+        private void OpenFile_btn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.FileName != string.Empty)
+                openPath = openFileDialog1.FileName;
         }
     }
 }
