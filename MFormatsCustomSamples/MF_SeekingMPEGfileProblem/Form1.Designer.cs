63ebd9b3e108c2f3612279cc2e7f77a9d966548a
tararonis <tararonis@gmail.com>
2019-07-15 18:22:30 +0200

    Sample that reproduces the seeking problem

---
+++ MFormatsCustomSamples/MF_SeekingMPEGfileProblem/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace MF_SeekingMPEGfileProblem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelPr = new System.Windows.Forms.Panel();
+            this.Open_btn = new System.Windows.Forms.Button();
+            this.panelFrame = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Stop_btn = new System.Windows.Forms.Button();
+            this.GetFrame_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelPr
+            // 
+            this.panelPr.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelPr.Location = new System.Drawing.Point(30, 52);
+            this.panelPr.Name = "panelPr";
+            this.panelPr.Size = new System.Drawing.Size(269, 174);
+            this.panelPr.TabIndex = 0;
+            // 
+            // Open_btn
+            // 
+            this.Open_btn.Location = new System.Drawing.Point(30, 260);
+            this.Open_btn.Name = "Open_btn";
+            this.Open_btn.Size = new System.Drawing.Size(269, 23);
+            this.Open_btn.TabIndex = 1;
+            this.Open_btn.Text = "OPen";
+            this.Open_btn.UseVisualStyleBackColor = true;
+            this.Open_btn.Click += new System.EventHandler(this.Open_btn_Click);
+            // 
+            // panelFrame
+            // 
+            this.panelFrame.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelFrame.Location = new System.Drawing.Point(350, 52);
+            this.panelFrame.Name = "panelFrame";
+            this.panelFrame.Size = new System.Drawing.Size(271, 183);
+            this.panelFrame.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Stop_btn
+            // 
+            this.Stop_btn.Location = new System.Drawing.Point(30, 288);
+            this.Stop_btn.Name = "Stop_btn";
+            this.Stop_btn.Size = new System.Drawing.Size(269, 23);
+            this.Stop_btn.TabIndex = 4;
+            this.Stop_btn.Text = "Stop";
+            this.Stop_btn.UseVisualStyleBackColor = true;
+            this.Stop_btn.Click += new System.EventHandler(this.Stop_btn_Click_1);
+            // 
+            // GetFrame_btn
+            // 
+            this.GetFrame_btn.Location = new System.Drawing.Point(350, 260);
+            this.GetFrame_btn.Name = "GetFrame_btn";
+            this.GetFrame_btn.Size = new System.Drawing.Size(271, 23);
+            this.GetFrame_btn.TabIndex = 5;
+            this.GetFrame_btn.Text = "GetFrame";
+            this.GetFrame_btn.UseVisualStyleBackColor = true;
+            this.GetFrame_btn.Click += new System.EventHandler(this.GetFrame_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GetFrame_btn);
+            this.Controls.Add(this.Stop_btn);
+            this.Controls.Add(this.panelFrame);
+            this.Controls.Add(this.Open_btn);
+            this.Controls.Add(this.panelPr);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelPr;
+        private System.Windows.Forms.Button Open_btn;
+        private System.Windows.Forms.Panel panelFrame;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Stop_btn;
+        private System.Windows.Forms.Button GetFrame_btn;
+    }
+}
+
---
+++ MFormatsCustomSamples/MF_SeekingMPEGfileProblem/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MFORMATSLib;
+
+namespace MF_SeekingMPEGfileProblem
+{
+    public partial class Form1 : Form
+    {
+        bool decode = false;
+        bool start = false;
+        MFPreviewClass m_objPreview;
+        MFPreviewClass m_objPreviewFrame;
+        MFReaderClass m_objReaderFrame;
+        MFReaderClass m_objReader;
+
+        private CancellationTokenSource cancelSource;
+        private Thread m_threadWorker;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_objPreview = new MFPreviewClass();
+            m_objPreviewFrame = new MFPreviewClass();
+
+            m_objReader = new MFReaderClass();
+            m_objReaderFrame = new MFReaderClass();            
+
+            m_objPreview.PreviewWindowSet("", panelPr.Handle.ToInt32());
+            m_objPreview.PreviewEnable("", 1, 1);
+
+            m_objPreviewFrame.PreviewWindowSet("", panelFrame.Handle.ToInt32());
+            m_objPreviewFrame.PreviewEnable("", 1, 1);
+
+            cancelSource = new CancellationTokenSource();
+            m_threadWorker = new Thread(() => thread_DoWork(cancelSource.Token));
+            m_threadWorker.Name = "thread_DoWork";
+            m_threadWorker.Start();
+        }
+
+        private void thread_DoWork(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                if (decode)
+                {
+                    MFFrame frame = null;
+
+                    M_AV_PROPS props = new M_AV_PROPS();
+                    props.vidProps.eVideoFormat = eMVideoFormat.eMVF_HD1080_60p;
+
+                    m_objReader.SourceFrameConvertedGet(ref props, -1, out frame, "");
+
+                    m_objPreview.ReceiverFramePut(frame, -1, "");
+
+                    Marshal.FinalReleaseComObject(frame);
+                }
+            }
+        }
+
+        string path;
+        void OpenFile()
+        {
+            openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.FileName != string.Empty)
+                path = openFileDialog1.FileName;
+
+            m_objReader.ReaderOpen(path, "");
+            
+
+            decode = true;
+
+        }
+
+        private void Open_btn_Click(object sender, EventArgs e)
+        {
+            start = start == false ? true : false;
+            if (decode)
+            {
+                decode = false;
+            }
+            if (start)
+                OpenFile();
+        }
+      
+        private void Stop_btn_Click_1(object sender, EventArgs e)
+        {
+            decode = false;
+            start = false;
+        }
+
+        private void GetFrame_btn_Click(object sender, EventArgs e)
+        {
+            m_objReaderFrame.ReaderOpen(path, "");
+            m_objReaderFrame.SourceAVPropsGet(out M_AV_PROPS props);
+            m_objReaderFrame.ReaderDurationGet(out double duration);
+
+            m_objReaderFrame.SourceFrameConvertedGetByTime(ref props, duration / 2, -1, out MFFrame frameTumbnail, "");
+
+            m_objPreviewFrame.ReceiverFramePut(frameTumbnail, -1, "");
+
+            Marshal.ReleaseComObject(frameTumbnail);
+        }
+    }
+}
