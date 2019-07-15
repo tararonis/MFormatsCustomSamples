079964f2d128584f836044467989490276f27b32
tararonis <tararonis@gmail.com>
2019-07-15 17:58:12 +0200

    Sample to reproduce the problem with Creating BitMap(ticket 32888)

---
+++ MFormatsCustomSamples/MF_CreateBitmap_32888/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace MF_CreateBitmap_32888
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelPrev = new System.Windows.Forms.Panel();
+            this.OpenFile_btn = new System.Windows.Forms.Button();
+            this.Stop_btn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // panelPrev
+            // 
+            this.panelPrev.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelPrev.Location = new System.Drawing.Point(57, 35);
+            this.panelPrev.Name = "panelPrev";
+            this.panelPrev.Size = new System.Drawing.Size(372, 246);
+            this.panelPrev.TabIndex = 0;
+            // 
+            // OpenFile_btn
+            // 
+            this.OpenFile_btn.Location = new System.Drawing.Point(57, 306);
+            this.OpenFile_btn.Name = "OpenFile_btn";
+            this.OpenFile_btn.Size = new System.Drawing.Size(197, 23);
+            this.OpenFile_btn.TabIndex = 1;
+            this.OpenFile_btn.Text = "Open";
+            this.OpenFile_btn.UseVisualStyleBackColor = true;
+            this.OpenFile_btn.Click += new System.EventHandler(this.OpenFile_btn_Click_1);
+            // 
+            // Stop_btn
+            // 
+            this.Stop_btn.Location = new System.Drawing.Point(57, 335);
+            this.Stop_btn.Name = "Stop_btn";
+            this.Stop_btn.Size = new System.Drawing.Size(197, 23);
+            this.Stop_btn.TabIndex = 2;
+            this.Stop_btn.Text = "Stop";
+            this.Stop_btn.UseVisualStyleBackColor = true;
+            this.Stop_btn.Click += new System.EventHandler(this.Stop_btn_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Stop_btn);
+            this.Controls.Add(this.OpenFile_btn);
+            this.Controls.Add(this.panelPrev);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelPrev;
+        private System.Windows.Forms.Button OpenFile_btn;
+        private System.Windows.Forms.Button Stop_btn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
