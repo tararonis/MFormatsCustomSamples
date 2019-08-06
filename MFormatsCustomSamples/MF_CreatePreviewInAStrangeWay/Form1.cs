51a693290016872988d1b1c1aa18319796c861b7
Roman Zalesinskiy <tararonis@gmail.com>
2019-08-06 11:29:44 +0200

    Sample that compare the speed of creating the MFPReaview object with var and object himself

---
+++ MFormatsCustomSamples/MF_CreatePreviewInAStrangeWay/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MF_CreatePreviewInAStrangeWay
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            TestVar();
+
+            TestObj();
+
+        }
+
+        private void TestObj()
+        {
+            DateTime before = DateTime.Now;
+            MFORMATSLib.MFPreviewClass a = new MFORMATSLib.MFPreviewClass();
+            DateTime after = DateTime.Now;
+
+            TimeSpan dif = after - before;
+            listBox1.Items.Add("First time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            MFORMATSLib.MFPreviewClass b = new MFORMATSLib.MFPreviewClass();
+            after = DateTime.Now;
+
+            dif = after - before;
+            listBox1.Items.Add("Second time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            MFORMATSLib.MFPreviewClass c = new MFORMATSLib.MFPreviewClass();
+            after = DateTime.Now;
+
+            dif = after - before;
+            listBox1.Items.Add("Third time we create: " + dif.TotalMilliseconds.ToString());
+        }
+
+        private void TestVar()
+        {
+            DateTime before = DateTime.Now;
+            var test = new MFORMATSLib.MFPreviewClass();
+            DateTime after = DateTime.Now;
+
+            TimeSpan dif = after - before;
+            listBox1.Items.Add("First time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            var test2 = new MFORMATSLib.MFPreviewClass();
+            after = DateTime.Now;
+
+            dif = after - before;
+            listBox1.Items.Add("Second time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            var test3 = new MFORMATSLib.MFPreviewClass();
+            after = DateTime.Now;
+
+            dif = after - before;
+            listBox1.Items.Add("Third time we create: " + dif.TotalMilliseconds.ToString());
+        }
+    }
+}
