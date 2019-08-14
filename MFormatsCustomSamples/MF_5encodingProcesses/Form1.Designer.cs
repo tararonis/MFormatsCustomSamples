c5ec7c1349bba6924549bdb881f9b665f0ee4c67
Roman Zalesinskiy <tararonis@gmail.com>
2019-08-14 18:09:21 +0200

    Sample 4 Encoding Test

---
+++ MFormatsCustomSamples/MF_5encodingProcesses/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace MF_5encodingProcesses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.Start1_btn = new System.Windows.Forms.Button();
+            this.Start2_btn = new System.Windows.Forms.Button();
+            this.Start3_btn = new System.Windows.Forms.Button();
+            this.Start4_btn = new System.Windows.Forms.Button();
+            this.Start5_btn = new System.Windows.Forms.Button();
+            this.EncodingConfigs_lsb = new System.Windows.Forms.ListBox();
+            this.Path1_txb = new System.Windows.Forms.TextBox();
+            this.Path2_txb = new System.Windows.Forms.TextBox();
+            this.Path3_txb = new System.Windows.Forms.TextBox();
+            this.Path4_txb = new System.Windows.Forms.TextBox();
+            this.Path5_txb = new System.Windows.Forms.TextBox();
+            this.SavePath_txb = new System.Windows.Forms.TextBox();
+            this.SelectSaveFolder_btn = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(248, 203);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Location = new System.Drawing.Point(374, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(248, 203);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Location = new System.Drawing.Point(750, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(248, 203);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel4.Location = new System.Drawing.Point(12, 330);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(248, 203);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel5.Location = new System.Drawing.Point(374, 330);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(248, 203);
+            this.panel5.TabIndex = 1;
+            // 
+            // Start1_btn
+            // 
+            this.Start1_btn.Enabled = false;
+            this.Start1_btn.Location = new System.Drawing.Point(12, 233);
+            this.Start1_btn.Name = "Start1_btn";
+            this.Start1_btn.Size = new System.Drawing.Size(247, 23);
+            this.Start1_btn.TabIndex = 2;
+            this.Start1_btn.Text = "Open And Start Playback";
+            this.Start1_btn.UseVisualStyleBackColor = true;
+            this.Start1_btn.Click += new System.EventHandler(this.Start1_btn_Click);
+            // 
+            // Start2_btn
+            // 
+            this.Start2_btn.Enabled = false;
+            this.Start2_btn.Location = new System.Drawing.Point(374, 233);
+            this.Start2_btn.Name = "Start2_btn";
+            this.Start2_btn.Size = new System.Drawing.Size(247, 23);
+            this.Start2_btn.TabIndex = 3;
+            this.Start2_btn.Text = "Open And Start Playback";
+            this.Start2_btn.UseVisualStyleBackColor = true;
+            this.Start2_btn.Click += new System.EventHandler(this.Start2_btn_Click);
+            // 
+            // Start3_btn
+            // 
+            this.Start3_btn.Enabled = false;
+            this.Start3_btn.Location = new System.Drawing.Point(750, 233);
+            this.Start3_btn.Name = "Start3_btn";
+            this.Start3_btn.Size = new System.Drawing.Size(247, 23);
+            this.Start3_btn.TabIndex = 4;
+            this.Start3_btn.Text = "Open And Start Playback";
+            this.Start3_btn.UseVisualStyleBackColor = true;
+            this.Start3_btn.Click += new System.EventHandler(this.Start3_btn_Click);
+            // 
+            // Start4_btn
+            // 
+            this.Start4_btn.Enabled = false;
+            this.Start4_btn.Location = new System.Drawing.Point(12, 540);
+            this.Start4_btn.Name = "Start4_btn";
+            this.Start4_btn.Size = new System.Drawing.Size(247, 23);
+            this.Start4_btn.TabIndex = 5;
+            this.Start4_btn.Text = "Open And Start Playback";
+            this.Start4_btn.UseVisualStyleBackColor = true;
+            this.Start4_btn.Click += new System.EventHandler(this.Start4_btn_Click);
+            // 
+            // Start5_btn
+            // 
+            this.Start5_btn.Enabled = false;
+            this.Start5_btn.Location = new System.Drawing.Point(374, 540);
+            this.Start5_btn.Name = "Start5_btn";
+            this.Start5_btn.Size = new System.Drawing.Size(247, 23);
+            this.Start5_btn.TabIndex = 6;
+            this.Start5_btn.Text = "Open And Start Playback";
+            this.Start5_btn.UseVisualStyleBackColor = true;
+            this.Start5_btn.Click += new System.EventHandler(this.Start5_btn_Click);
+            // 
+            // EncodingConfigs_lsb
+            // 
+            this.EncodingConfigs_lsb.FormattingEnabled = true;
+            this.EncodingConfigs_lsb.Location = new System.Drawing.Point(648, 330);
+            this.EncodingConfigs_lsb.Name = "EncodingConfigs_lsb";
+            this.EncodingConfigs_lsb.Size = new System.Drawing.Size(422, 160);
+            this.EncodingConfigs_lsb.TabIndex = 7;
+            // 
+            // Path1_txb
+            // 
+            this.Path1_txb.Location = new System.Drawing.Point(12, 275);
+            this.Path1_txb.Name = "Path1_txb";
+            this.Path1_txb.ReadOnly = true;
+            this.Path1_txb.Size = new System.Drawing.Size(247, 20);
+            this.Path1_txb.TabIndex = 8;
+            // 
+            // Path2_txb
+            // 
+            this.Path2_txb.Location = new System.Drawing.Point(374, 275);
+            this.Path2_txb.Name = "Path2_txb";
+            this.Path2_txb.ReadOnly = true;
+            this.Path2_txb.Size = new System.Drawing.Size(247, 20);
+            this.Path2_txb.TabIndex = 9;
+            // 
+            // Path3_txb
+            // 
+            this.Path3_txb.Location = new System.Drawing.Point(751, 275);
+            this.Path3_txb.Name = "Path3_txb";
+            this.Path3_txb.ReadOnly = true;
+            this.Path3_txb.Size = new System.Drawing.Size(247, 20);
+            this.Path3_txb.TabIndex = 10;
+            // 
+            // Path4_txb
+            // 
+            this.Path4_txb.Location = new System.Drawing.Point(12, 578);
+            this.Path4_txb.Name = "Path4_txb";
+            this.Path4_txb.ReadOnly = true;
+            this.Path4_txb.Size = new System.Drawing.Size(247, 20);
+            this.Path4_txb.TabIndex = 11;
+            // 
+            // Path5_txb
+            // 
+            this.Path5_txb.Location = new System.Drawing.Point(374, 578);
+            this.Path5_txb.Name = "Path5_txb";
+            this.Path5_txb.ReadOnly = true;
+            this.Path5_txb.Size = new System.Drawing.Size(247, 20);
+            this.Path5_txb.TabIndex = 12;
+            // 
+            // SavePath_txb
+            // 
+            this.SavePath_txb.Location = new System.Drawing.Point(648, 512);
+            this.SavePath_txb.Name = "SavePath_txb";
+            this.SavePath_txb.Size = new System.Drawing.Size(422, 20);
+            this.SavePath_txb.TabIndex = 13;
+            // 
+            // SelectSaveFolder_btn
+            // 
+            this.SelectSaveFolder_btn.Location = new System.Drawing.Point(648, 539);
+            this.SelectSaveFolder_btn.Name = "SelectSaveFolder_btn";
+            this.SelectSaveFolder_btn.Size = new System.Drawing.Size(422, 23);
+            this.SelectSaveFolder_btn.TabIndex = 14;
+            this.SelectSaveFolder_btn.Text = "Save Folder";
+            this.SelectSaveFolder_btn.UseVisualStyleBackColor = true;
+            this.SelectSaveFolder_btn.Click += new System.EventHandler(this.SelectSaveFolder_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1094, 631);
+            this.Controls.Add(this.SelectSaveFolder_btn);
+            this.Controls.Add(this.SavePath_txb);
+            this.Controls.Add(this.Path5_txb);
+            this.Controls.Add(this.Path4_txb);
+            this.Controls.Add(this.Path3_txb);
+            this.Controls.Add(this.Path2_txb);
+            this.Controls.Add(this.Path1_txb);
+            this.Controls.Add(this.EncodingConfigs_lsb);
+            this.Controls.Add(this.Start5_btn);
+            this.Controls.Add(this.Start4_btn);
+            this.Controls.Add(this.Start3_btn);
+            this.Controls.Add(this.Start2_btn);
+            this.Controls.Add(this.Start1_btn);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button Start1_btn;
+        private System.Windows.Forms.Button Start2_btn;
+        private System.Windows.Forms.Button Start3_btn;
+        private System.Windows.Forms.Button Start4_btn;
+        private System.Windows.Forms.Button Start5_btn;
+        private System.Windows.Forms.ListBox EncodingConfigs_lsb;
+        private System.Windows.Forms.TextBox Path1_txb;
+        private System.Windows.Forms.TextBox Path2_txb;
+        private System.Windows.Forms.TextBox Path3_txb;
+        private System.Windows.Forms.TextBox Path4_txb;
+        private System.Windows.Forms.TextBox Path5_txb;
+        private System.Windows.Forms.TextBox SavePath_txb;
+        private System.Windows.Forms.Button SelectSaveFolder_btn;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ MFormatsCustomSamples/MF_5encodingProcesses/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MFORMATSLib;
+
+namespace MF_5encodingProcesses
+{
+    public partial class Form1 : Form
+    {
+        string[] listOfEncodingConfigurations = {
+            " format='mp4' video::codec='n264' audio::codec='aac'",
+        };
+
+        private CancellationTokenSource cancelSource;
+        private Thread m_threadWorker;
+
+        MFReaderClass m_objReader1;
+        MFReaderClass m_objReader2;
+        MFReaderClass m_objReader3;
+        MFReaderClass m_objReader4;
+        MFReaderClass m_objReader5;
+
+        MFPreviewClass m_objPreview1;
+        MFPreviewClass m_objPreview2;
+        MFPreviewClass m_objPreview3;
+        MFPreviewClass m_objPreview4;
+        MFPreviewClass m_objPreview5;
+
+        MFWriterClass m_objWriter1;
+        MFWriterClass m_objWriter2;
+        MFWriterClass m_objWriter3;
+        MFWriterClass m_objWriter4;
+        MFWriterClass m_objWriter5;
+
+        bool start1 = false;
+        bool start2 = false;
+        bool start3 = false;
+        bool start4 = false;
+        bool start5 = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            m_objReader1 = new MFReaderClass();
+            m_objReader2 = new MFReaderClass();
+            m_objReader3 = new MFReaderClass();
+            m_objReader4 = new MFReaderClass();
+            m_objReader5 = new MFReaderClass();
+
+            m_objPreview1 = new MFPreviewClass();
+            m_objPreview1.PreviewWindowSet("",panel1.Handle.ToInt32());
+            m_objPreview1.PreviewEnable("", 0, 1);
+
+            m_objPreview2 = new MFPreviewClass();
+            m_objPreview2.PreviewWindowSet("", panel2.Handle.ToInt32());
+            m_objPreview2.PreviewEnable("", 0, 1);
+
+            m_objPreview3 = new MFPreviewClass();
+            m_objPreview3.PreviewWindowSet("", panel3.Handle.ToInt32());
+            m_objPreview3.PreviewEnable("", 0, 1);
+
+            m_objPreview4 = new MFPreviewClass();
+            m_objPreview4.PreviewWindowSet("", panel4.Handle.ToInt32());
+            m_objPreview4.PreviewEnable("", 0, 1);
+
+            m_objPreview5 = new MFPreviewClass();
+            m_objPreview5.PreviewWindowSet("", panel5.Handle.ToInt32());
+            m_objPreview5.PreviewEnable("", 0, 1);
+
+            m_objWriter1 = new MFWriterClass();
+            m_objWriter2 = new MFWriterClass();
+            m_objWriter3 = new MFWriterClass();
+            m_objWriter4 = new MFWriterClass();
+            m_objWriter5 = new MFWriterClass();
+
+            cancelSource = new CancellationTokenSource();
+            m_threadWorker = new Thread(() => thread_DoWork(cancelSource.Token))
+            {
+                Name = "thread_DoWork"
+            };
+            m_threadWorker.Start();
+
+            FillListBox();
+        }
+
+        private void thread_DoWork(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                MFFrame frame1 = null;
+                MFFrame frame2 = null;
+                MFFrame frame3 = null;
+                MFFrame frame4 = null;
+                MFFrame frame5 = null;
+
+                M_AV_PROPS props = new M_AV_PROPS();
+
+                props.vidProps.eInterlace = eMInterlace.eMI_Progressive;
+                if (start1)
+                {
+                    m_objReader1.SourceFrameConvertedGet(ref props, -1, out frame1, "");
+                    m_objWriter1.ReceiverFramePut(frame1, -1, "");
+                    m_objPreview1.ReceiverFramePut(frame1, -1, "");
+                }
+                if (start2)
+                {
+                    m_objReader2.SourceFrameConvertedGet(ref props, -1, out frame2, "");
+                    m_objWriter2.ReceiverFramePut(frame2, -1, "");
+                    m_objPreview2.ReceiverFramePut(frame2, -1, "");
+                }
+                if (start3)
+                {
+                    m_objReader3.SourceFrameConvertedGet(ref props, -1, out frame3, "");
+                    m_objWriter3.ReceiverFramePut(frame3, -1, "");
+                    m_objPreview3.ReceiverFramePut(frame3, -1, "");
+                }
+                if (start4)
+                {
+                    m_objReader4.SourceFrameConvertedGet(ref props, -1, out frame4, "");
+                    m_objWriter4.ReceiverFramePut(frame4, -1, "");
+                    m_objPreview4.ReceiverFramePut(frame4, -1, "");
+                }
+                if (start5)
+                {
+                    m_objReader5.SourceFrameConvertedGet(ref props, -1, out frame5, "");
+                    m_objWriter5.ReceiverFramePut(frame5,-1, "");
+                    m_objPreview5.ReceiverFramePut(frame5, -1, "");
+                }
+
+                if (frame1 != null)
+                    Marshal.ReleaseComObject(frame1);
+                if (frame2 != null)
+                    Marshal.ReleaseComObject(frame2);
+                if (frame3 != null)
+                    Marshal.ReleaseComObject(frame3);
+                if (frame4 != null)
+                    Marshal.ReleaseComObject(frame4);
+                if (frame5 != null)
+                    Marshal.ReleaseComObject(frame5);
+            }
+        }
+
+        private void Start1_btn_Click(object sender, EventArgs e)
+        {            
+            if (!start1)
+            {
+                OpenFileDialog a = new OpenFileDialog();
+
+                if (a.ShowDialog() == DialogResult.OK)
+                {
+                    m_objReader1.ReaderOpen(a.FileName, "loop=true");
+
+                    m_objWriter1.WriterSet(SavePath_txb.Text + "\\1.mp4", 0, EncodingConfigs_lsb.SelectedItem.ToString());
+
+
+                    Path1_txb.Text = a.FileName;
+                    start1 = !start1;
+                    Start1_btn.BackColor = start1 ? Color.Green : Color.Red;                    
+                }
+            }
+            else if (start1)
+            {                
+                start1 = !start1;
+                Thread.Sleep(10);
+                m_objWriter1.WriterClose(0);
+                Start1_btn.BackColor = start1 ? Color.Green : Color.Red;                
+            }
+
+        }
+
+        private void Start2_btn_Click(object sender, EventArgs e)
+        {
+            if (!start2)
+            {
+                OpenFileDialog a = new OpenFileDialog();
+
+                if (a.ShowDialog() == DialogResult.OK)
+                {
+                    m_objReader2.ReaderOpen(a.FileName, "loop=true");
+
+                    m_objWriter2.WriterSet(SavePath_txb.Text + "\\2.mp4", 0, EncodingConfigs_lsb.SelectedItem.ToString());
+
+
+                    Path2_txb.Text = a.FileName;
+                    start2 = !start2;
+                    Start2_btn.BackColor = start2 ? Color.Green : Color.Red;
+                }
+            }
+            else if (start2)
+            {
+                start2 = !start2;
+                Thread.Sleep(10);
+                m_objWriter2.WriterClose(0);
+                Start2_btn.BackColor = start2 ? Color.Green : Color.Red;
+            }
+        }
+
+        private void Start3_btn_Click(object sender, EventArgs e)
+        {
+            if (!start3)
+            {
+                OpenFileDialog a = new OpenFileDialog();
+
+                if (a.ShowDialog() == DialogResult.OK)
+                {
+                    m_objReader3.ReaderOpen(a.FileName, "loop=true");
+
+                    m_objWriter3.WriterSet(SavePath_txb.Text + "\\3.mp4", 0, EncodingConfigs_lsb.SelectedItem.ToString());
+
+
+                    Path3_txb.Text = a.FileName;
+                    start3 = !start3;
+                    Start3_btn.BackColor = start3 ? Color.Green : Color.Red;
+                }
+            }
+            else if (start3)
+            {
+                start3 = !start3;
+                Thread.Sleep(10);
+                m_objWriter3.WriterClose(0);
+                Start3_btn.BackColor = start3 ? Color.Green : Color.Red;
+            }
+        }
+
+        private void Start4_btn_Click(object sender, EventArgs e)
+        {
+            if (!start4)
+            {
+                OpenFileDialog a = new OpenFileDialog();
+
+                if (a.ShowDialog() == DialogResult.OK)
+                {
+                    m_objReader4.ReaderOpen(a.FileName, "loop=true");
+
+                    m_objWriter4.WriterSet(SavePath_txb.Text + "\\4.mp4", 0, EncodingConfigs_lsb.SelectedItem.ToString());
+
+
+                    Path4_txb.Text = a.FileName;
+                    start4 = !start4;
+                    Start4_btn.BackColor = start4 ? Color.Green : Color.Red;
+                }
+            }
+            else if (start4)
+            {
+                start4 = !start4;
+                Thread.Sleep(10);
+                m_objWriter4.WriterClose(0);
+                Start4_btn.BackColor = start4 ? Color.Green : Color.Red;
+            }
+        }
+
+        private void Start5_btn_Click(object sender, EventArgs e)
+        {
+            if (!start5)
+            {
+                OpenFileDialog a = new OpenFileDialog();
+
+                if (a.ShowDialog() == DialogResult.OK)
+                {
+                    m_objReader5.ReaderOpen(a.FileName, "loop=true");
+
+                    m_objWriter5.WriterSet(SavePath_txb.Text + "\\5.mp4", 0, EncodingConfigs_lsb.SelectedItem.ToString());
+
+
+                    Path5_txb.Text = a.FileName;
+                    start5 = !start5;
+                    Start5_btn.BackColor = start5 ? Color.Green : Color.Red;
+                }
+            }
+            else if (start5)
+            {
+                start5 = !start5;
+                Thread.Sleep(10);
+                m_objWriter5.WriterClose(0);
+                Start5_btn.BackColor = start5 ? Color.Green : Color.Red;
+            }
+        }
+
+        private void FillListBox()
+        {
+            foreach (var c in listOfEncodingConfigurations)
+            {
+                EncodingConfigs_lsb.Items.Add(c);
+            }
+            EncodingConfigs_lsb.SelectedIndex = 0;
+        }
+
+        private void SelectSaveFolder_btn_Click(object sender, EventArgs e)
+        {  
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                SavePath_txb.Text = folderBrowserDialog1.SelectedPath;
+
+                Start1_btn.Enabled = true;
+                Start2_btn.Enabled = true;
+                Start3_btn.Enabled = true;
+                Start4_btn.Enabled = true;
+                Start5_btn.Enabled = true;
+            }
+        }
+    }
+}
