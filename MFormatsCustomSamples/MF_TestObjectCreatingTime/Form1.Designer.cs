53d19d1ab56831ac98935b8c74d5bbd67f906ff7
Roman Zalesinskiy <tararonis@gmail.com>
2019-08-29 17:10:09 +0200

    MP-1298 Sample illustrates issue on first time object creating

---
+++ MFormatsCustomSamples/MF_TestObjectCreatingTime/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MF_TestObjectCreatingTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Stat_lsb = new System.Windows.Forms.ListBox();
+            this.Objects_lsb = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Stat_lsb
+            // 
+            this.Stat_lsb.FormattingEnabled = true;
+            this.Stat_lsb.Location = new System.Drawing.Point(12, 35);
+            this.Stat_lsb.Name = "Stat_lsb";
+            this.Stat_lsb.Size = new System.Drawing.Size(269, 355);
+            this.Stat_lsb.TabIndex = 0;
+            // 
+            // Objects_lsb
+            // 
+            this.Objects_lsb.FormattingEnabled = true;
+            this.Objects_lsb.Location = new System.Drawing.Point(296, 35);
+            this.Objects_lsb.Name = "Objects_lsb";
+            this.Objects_lsb.Size = new System.Drawing.Size(120, 173);
+            this.Objects_lsb.TabIndex = 1;
+            this.Objects_lsb.SelectedIndexChanged += new System.EventHandler(this.Objects_lsb_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SelectObj";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 402);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Objects_lsb);
+            this.Controls.Add(this.Stat_lsb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Stat_lsb;
+        private System.Windows.Forms.ListBox Objects_lsb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MFormatsCustomSamples/MF_TestObjectCreatingTime/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MFORMATSLib;
+
+namespace MF_TestObjectCreatingTime
+{
+    public partial class Form1 : Form
+    {
+
+        DateTime before;
+        DateTime after;
+        TimeSpan dif;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Objects_lsb.Items.Add("Preview");
+            Objects_lsb.Items.Add("Writer");
+            Objects_lsb.Items.Add("Reader");
+            Objects_lsb.Items.Add("Live");
+            Objects_lsb.Items.Add("WebRTC");
+        }
+        
+
+        private void Objects_lsb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox comboBox = (ListBox)sender;
+            int selected = (int)comboBox.SelectedIndex;
+            if (selected == 0)
+            {
+                CreatePreviews();
+            }
+            else if (selected == 1)
+            {
+                CreateWriters();
+            }
+            else if (selected == 2)
+            {
+                CreateReaders();
+            }
+            else if (selected == 3)
+            {
+                CreateLives();
+            }
+            else if (selected == 4)
+            {
+                CreateWebRTCs();
+            }
+        }
+
+        private void CreateWebRTCs()
+        {
+            before = DateTime.Now;
+            MFORMATSLib.MWebRTCClass a = new MFORMATSLib.MWebRTCClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("First time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            MFORMATSLib.MWebRTCClass b = new MFORMATSLib.MWebRTCClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("Second time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            MFORMATSLib.MWebRTCClass c = new MFORMATSLib.MWebRTCClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("Third time we create: " + dif.TotalMilliseconds.ToString());
+        }
+
+        private void CreateLives()
+        {
+            before = DateTime.Now;
+            MFORMATSLib.MFLiveClass a = new MFORMATSLib.MFLiveClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("First time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            MFORMATSLib.MFLiveClass b = new MFORMATSLib.MFLiveClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("Second time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            MFORMATSLib.MFLiveClass c = new MFORMATSLib.MFLiveClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("Third time we create: " + dif.TotalMilliseconds.ToString());
+        }
+
+
+        private void CreateReaders()
+        {
+            before = DateTime.Now;
+            MFORMATSLib.MFReaderClass a = new MFORMATSLib.MFReaderClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("First time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            MFORMATSLib.MFReaderClass b = new MFORMATSLib.MFReaderClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("Second time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            MFORMATSLib.MFReaderClass c = new MFORMATSLib.MFReaderClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("Third time we create: " + dif.TotalMilliseconds.ToString());
+        }
+
+        private void CreateWriters()
+        {
+            before = DateTime.Now;
+            MFORMATSLib.MFWriterClass a = new MFORMATSLib.MFWriterClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("First time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            MFORMATSLib.MFWriterClass b = new MFORMATSLib.MFWriterClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("Second time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            MFORMATSLib.MFWriterClass c = new MFORMATSLib.MFWriterClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("Third time we create: " + dif.TotalMilliseconds.ToString());
+        }
+
+
+
+        private void CreatePreviews()
+        {
+            before = DateTime.Now;
+            MFORMATSLib.MFPreviewClass a = new MFORMATSLib.MFPreviewClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("First time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            MFORMATSLib.MFPreviewClass b = new MFORMATSLib.MFPreviewClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("Second time we create: " + dif.TotalMilliseconds.ToString());
+
+            before = DateTime.Now;
+            MFORMATSLib.MFPreviewClass c = new MFORMATSLib.MFPreviewClass();
+            after = DateTime.Now;
+            dif = after - before;
+            Stat_lsb.Items.Add("Third time we create: " + dif.TotalMilliseconds.ToString());
+        }
+    }
+}
+
